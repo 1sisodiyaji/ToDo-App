da1030c5f2b118285428588bc1c807da808dfca6
1sisodiyaji <637golusingh@gmail.com>
2023-10-02 01:50:26 +0530

    commit

---
+++ controllers/createTodo.js
@@ -0,0 +1,23 @@
+const Todo = require("../models/Todo");
+
+exports.createTodo = async(req,res) => {
+    try {
+            const{title,description} = req.body;
+            const response = await Todo.create({title,description});
+            res.status(200).json({
+                success:true,
+                data:response,
+                message:'Entry Created Successfully'
+            })
+    }
+    catch(error){
+        console.error(error);
+        console.log(error);
+        res.status(400)
+        .json({
+            success:false,
+            data:"Internal Server Issue",
+            message:error.message,
+        })
+    }
+}
